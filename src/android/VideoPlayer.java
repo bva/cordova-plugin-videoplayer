8e4dff9d935103228d5c8e1072473c956c96a4e0
Vitaliy Batichko <vbatichko@gmail.com>
2017-07-10 23:46:15 +0300

    Refactor player to activity

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -1,40 +1,74 @@
 package com.moust.cordova.videoplayer;
 
-import android.annotation.TargetApi;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnDismissListener;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
-import android.media.MediaPlayer.OnErrorListener;
-import android.media.MediaPlayer.OnPreparedListener;
-import android.widget.MediaController;
-import android.net.Uri;
-import android.os.Build;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.widget.VideoView;
 import android.content.Context;
 import android.content.Intent;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaResourceApi;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.support.v4.content.LocalBroadcastManager;
 
-public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, OnPreparedListener, OnErrorListener, OnDismissListener {
+public class VideoPlayer extends CordovaPlugin {
+    private CallbackContext callbackContext;
+    private CallbackContext playCallbackContext;
 
-    protected static final String LOG_TAG = "VideoPlayer";
+    private LocalBroadcastManager localBroadcastManager;
 
-    private CallbackContext callbackContext = null;
+    private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
 
-    private VideoView videoView;
-    private MediaPlayer player;
-    private MediaController controller;
+            String action = intent.getStringExtra("action");
+            Log.d(LOG_TAG, "Message received: " + action);
 
-    private Integer duration = -1;
+            if (action.equals("play")) {
+                String event = intent.getStringExtra("event");
+                Log.d(LOG_TAG, "Play event: " + event);
+
+                PluginResult pluginResult = null;
+
+                if (event.equals("error")) {
+                    pluginResult = new PluginResult(PluginResult.Status.ERROR, intent.getStringExtra("error"));
+                    pluginResult.setKeepCallback(false);
+                } else {
+                    pluginResult = new PluginResult(PluginResult.Status.OK, event);
+                    pluginResult.setKeepCallback(true);
+                }
+
+                playCallbackContext.sendPluginResult(pluginResult);
+            }
+            else if (action.equals("getCurrentPosition")) {
+                Integer position = intent.getIntExtra("currentPosition", -1);
+                Log.d(LOG_TAG, "Position: " + position);
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, position.toString());
+                callbackContext.sendPluginResult(pluginResult);
+                callbackContext = null;
+            }
+            else if(action.equals("getDuration")) {
+                Integer duration = intent.getIntExtra("duration", -1);
+                Log.d(LOG_TAG, "Duration: " + duration);
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, duration.toString());
+                callbackContext.sendPluginResult(pluginResult);
+                callbackContext = null;
+            }
+        }
+    };
+
+    private static final String LOG_TAG = VideoPlayer.class.getSimpleName();
+
+    @Override
+    protected void pluginInitialize() {
+        super.pluginInitialize();
+        localBroadcastManager = LocalBroadcastManager.getInstance(cordova.getActivity().getApplicationContext());
+        localBroadcastManager.registerReceiver(messageReceiver, new IntentFilter(this.getClass().getSimpleName()));
+    }
 
     /**
      * Executes the request and returns PluginResult.
@@ -45,207 +79,67 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
      * @return                   A PluginResult object with a status and message.
      */
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("play")) {
-            this.callbackContext = callbackContext;
+        Log.d(LOG_TAG, "Execute action: " + action);
 
-            CordovaResourceApi resourceApi = webView.getResourceApi();
-            String target = args.getString(0);
+        if (action.equals("play")) {
+            final String target = args.getString(0);
             final JSONObject options = args.getJSONObject(1);
 
-            String fileUriStr;
-            try {
-                Uri targetUri = resourceApi.remapUri(Uri.parse(target));
-                fileUriStr = targetUri.toString();
-            } catch (IllegalArgumentException e) {
-                fileUriStr = target;
-            }
+            final Activity cordovaActivity = cordova.getActivity();
 
-            Log.v(LOG_TAG, fileUriStr);
-
-            final String path = fileUriStr;
-
-            // Create dialog in new thread
-            cordova.getActivity().runOnUiThread(new Runnable() {
+            cordovaActivity.runOnUiThread(new Runnable() {
                 public void run() {
-                    Context context = cordova.getActivity().getApplicationContext();
-                    openNewActivity(context);
+                    openNewActivity(cordovaActivity.getApplicationContext(), target, options);
                 }
 
-                /*                public void run() {
-                    openVideoDialog(path, options);
-                } */
             });
 
-            // Don't return any result now
-            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-            pluginResult.setKeepCallback(true);
-            callbackContext.sendPluginResult(pluginResult);
-            callbackContext = null;
-
+            playCallbackContext = callbackContext;
             return true;
         }
         else if (action.equals("getCurrentPosition")) {
             this.callbackContext = callbackContext;
 
-            Integer position = 0;
-
-            if(player != null) {
-                position = player.getCurrentPosition();
-            }
-
-            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, position.toString());
-            pluginResult.setKeepCallback(true);
-            callbackContext.sendPluginResult(pluginResult);
-            callbackContext = null;
+            Intent intent = new Intent(VideoPlayerActivity.class.getSimpleName());
+            intent.putExtra("action", action);
+            localBroadcastManager.sendBroadcast(intent);
             return true;
         }
         else if (action.equals("getDuration")) {
             this.callbackContext = callbackContext;
-            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, duration.toString());
-            pluginResult.setKeepCallback(true);
-            callbackContext.sendPluginResult(pluginResult);
-            callbackContext = null;
+
+            Intent intent = new Intent(VideoPlayerActivity.class.getSimpleName());
+            intent.putExtra("action", action);
+            localBroadcastManager.sendBroadcast(intent);
             return true;
         }
         else if (action.equals("close")) {
-            if(player.isPlaying()) {
-                player.stop();
-            }
-            player.release();
+            this.callbackContext = callbackContext;
 
-            if (callbackContext != null) {
-                PluginResult result = new PluginResult(PluginResult.Status.OK);
-                result.setKeepCallback(false); // release status callback in JS side
-                callbackContext.sendPluginResult(result);
-                callbackContext = null;
-            }
-
+            Intent intent = new Intent(VideoPlayerActivity.class.getSimpleName());
+            intent.putExtra("action", action);
+            localBroadcastManager.sendBroadcast(intent);
             return true;
         }
+
         return false;
     }
 
-    private void openNewActivity(Context context) {
+    private void openNewActivity(Context context, String path, JSONObject options) {
         Intent intent = new Intent(context, VideoPlayerActivity.class);
+        intent.putExtra("url", path);
+        try {
+            intent.putExtra("volume", options.getDouble("volume"));
+            intent.putExtra("scalingMode", options.getInt("scalingMode"));
+        } catch(JSONException e) {
+            Log.e(LOG_TAG, "Error parsing options", e);
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "Error parsing options: " + e.getMessage());
+
+            pluginResult.setKeepCallback(false);
+            playCallbackContext.sendPluginResult(pluginResult);
+            return;
+        }
+
         this.cordova.getActivity().startActivity(intent);
-    }
-
-/*
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    protected void openVideoDialog(String path, JSONObject options) {
-        videoView = new VideoView(cordova.getActivity());
-        cordova.getActivity().setContentView(videoView);
-
-        player = new MediaPlayer();
-        player.setOnPreparedListener(this);
-        player.setOnCompletionListener(this);
-        player.setOnErrorListener(this);
-
-        try {
-            player.setDataSource(path);
-        } catch (Exception e) {
-            PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-            result.setKeepCallback(false); // release status callback in JS side
-            callbackContext.sendPluginResult(result);
-            callbackContext = null;
-            return;
-        }
-
-        try {
-            float volume = Float.valueOf(options.getString("volume"));
-            Log.d(LOG_TAG, "setVolume: " + volume);
-            player.setVolume(volume, volume);
-        } catch (Exception e) {
-            PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-            result.setKeepCallback(false); // release status callback in JS side
-            callbackContext.sendPluginResult(result);
-            callbackContext = null;
-            return;
-        }
-
-        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-            try {
-                int scalingMode = options.getInt("scalingMode");
-                switch (scalingMode) {
-                    case MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING:
-                        Log.d(LOG_TAG, "setVideoScalingMode VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING");
-                        player.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
-                        break;
-                    default:
-                        Log.d(LOG_TAG, "setVideoScalingMode VIDEO_SCALING_MODE_SCALE_TO_FIT");
-                        player.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT);
-                }
-            } catch (Exception e) {
-                PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-                result.setKeepCallback(false); // release status callback in JS side
-                callbackContext.sendPluginResult(result);
-                callbackContext = null;
-                return;
-            }
-        }
-
-        final SurfaceHolder mHolder = videoView.getHolder();
-        mHolder.setKeepScreenOn(true);
-        mHolder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                player.setDisplay(holder);
-                try {
-                    player.prepare();
-                } catch (Exception e) {
-                    PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-                    result.setKeepCallback(false); // release status callback in JS side
-                    callbackContext.sendPluginResult(result);
-                    callbackContext = null;
-                }
-            }
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                player.release();
-            }
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
-        });
-    }
-*/
-
-    @Override
-    public boolean onError(MediaPlayer mp, int what, int extra) {
-        Log.e(LOG_TAG, "MediaPlayer.onError(" + what + ", " + extra + ")");
-        if(mp.isPlaying()) {
-            mp.stop();
-        }
-        mp.release();
-        duration = -1;
-        return false;
-    }
-
-    @Override
-    public void onPrepared(MediaPlayer mp) {
-        mp.start();
-        videoView.requestFocus();
-        controller = new MediaController(cordova.getActivity());
-        videoView.setMediaController(controller);
-        controller.show(1000000);
-        duration = player.getDuration();
-        Log.d(LOG_TAG, "MediaPlayer started");
-    }
-
-    @Override
-    public void onCompletion(MediaPlayer mp) {
-        Log.d(LOG_TAG, "MediaPlayer completed");
-        mp.release();
-        duration = -1;
-    }
-
-    @Override
-    public void onDismiss(DialogInterface dialog) {
-        Log.d(LOG_TAG, "Dialog dismissed");
-        if (callbackContext != null) {
-            PluginResult result = new PluginResult(PluginResult.Status.OK);
-            result.setKeepCallback(false); // release status callback in JS side
-            callbackContext.sendPluginResult(result);
-            callbackContext = null;
-        }
     }
 }
--- src/android/VideoPlayerActivity.java
+++ src/android/VideoPlayerActivity.java
@@ -1,14 +1,202 @@
 package com.moust.cordova.videoplayer;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class VideoPlayerActivity extends Activity {
+import org.apache.cordova.PluginResult;
+
+public class VideoPlayerActivity extends Activity implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaController.MediaPlayerControl {
+    private static final String LOG_TAG = VideoPlayerActivity.class.getSimpleName();
+
+    private LocalBroadcastManager localBroadcastManager;
+    private VideoView videoView;
+    private MediaController controller;
+
+    private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getStringExtra("action");
+            Intent response_intent = new Intent(VideoPlayer.class.getSimpleName());
+            response_intent.putExtra("action", action);
+
+            Log.d(LOG_TAG, "Message received: " + action);
+
+            if(action.equals("getCurrentPosition")) {
+                response_intent.putExtra("currentPosition", getCurrentPosition());
+                localBroadcastManager.sendBroadcast(response_intent);
+            }
+            else if(action.equals("getDuration")) {
+                response_intent.putExtra("duration", getDuration());
+                localBroadcastManager.sendBroadcast(response_intent);
+            } else if(action.equals("close")) {
+                if(videoView.isPlaying()) {
+                    videoView.stopPlayback();
+                }
+
+                response_intent.putExtra("action", "play");
+                response_intent.putExtra("event", "closed");
+                localBroadcastManager.sendBroadcast(response_intent);
+
+                finish();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String package_name = getApplication().getPackageName();
-        setContentView(getApplication().getResources().getIdentifier("video_player_activity", "layout", package_name));
+        setContentView(getResources().getIdentifier("video_player_activity", "layout", package_name));
+        int id = getResources().getIdentifier("videoview", "id", package_name);
+
+        videoView = (VideoView) findViewById(id);
+
+        controller = new MediaController(this);
+        controller.setMediaPlayer(this);
+        controller.setAnchorView(videoView);
+        controller.setMediaPlayer(videoView);
+        videoView.setMediaController(controller);
+        videoView.setOnPreparedListener(this);
+        videoView.setOnCompletionListener(this);
+        videoView.setOnErrorListener(this);
+
+        Intent intent = getIntent();
+        videoView.setVideoPath(intent.getStringExtra("url"));
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        localBroadcastManager.registerReceiver(messageReceiver, new IntentFilter(this.getClass().getSimpleName()));
+    }
+
+    public void start() {
+        videoView.start();
+
+        Intent response_intent = new Intent(VideoPlayer.class.getSimpleName());
+        response_intent.putExtra("action", "play");
+        response_intent.putExtra("event", "started");
+        localBroadcastManager.sendBroadcast(response_intent);
+        Log.d(LOG_TAG, "MediaPlayer started");
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+
+    public void pause() {
+        videoView.pause();
+
+        Intent response_intent = new Intent(VideoPlayer.class.getSimpleName());
+        response_intent.putExtra("action", "play");
+        response_intent.putExtra("event", "paused");
+        localBroadcastManager.sendBroadcast(response_intent);
+        Log.d(LOG_TAG, "MediaPlayer paused");
+    }
+
+    public int getDuration() {
+        return videoView.getDuration();
+    }
+
+    public int getCurrentPosition() {
+        return videoView.getCurrentPosition();
+    }
+
+    public void seekTo(int i) {
+        videoView.seekTo(i);
+    }
+
+    public boolean isPlaying() {
+        return videoView.isPlaying();
+    }
+
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    public boolean canPause() {
+        return videoView.canPause();
+    }
+
+    public boolean canSeekBackward() {
+        return videoView.canSeekBackward();
+    }
+
+    public boolean canSeekForward() {
+        return videoView.canSeekForward();
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        Log.e(LOG_TAG, "MediaPlayer.onError(" + what + ", " + extra + ")");
+        if(mp.isPlaying()) {
+            mp.stop();
+        }
+
+        mp.release();
+
+        Intent response_intent = new Intent(VideoPlayer.class.getSimpleName());
+        response_intent.putExtra("action", "play");
+        response_intent.putExtra("event", "error");
+        response_intent.putExtra("error", "MediaPlayer.onError(" + what + ", " + extra + ")");
+        localBroadcastManager.sendBroadcast(response_intent);
+
+        finish();
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Intent intent = getIntent();
+
+        float volume = (float)intent.getDoubleExtra("volume", -1);
+
+        if(volume != -1) {
+            Log.d(LOG_TAG, "setVolume: " + volume);
+            mp.setVolume(volume, volume);
+        }
+
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            int scalingMode = intent.getIntExtra("scalingMode", -1);
+
+            switch (scalingMode) {
+                case MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING:
+                    Log.d(LOG_TAG, "setVideoScalingMode VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING");
+                    mp.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+                    break;
+                default:
+                    Log.d(LOG_TAG, "setVideoScalingMode VIDEO_SCALING_MODE_SCALE_TO_FIT");
+                    mp.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT);
+            }
+        }
+
+        Intent response_intent = new Intent(VideoPlayer.class.getSimpleName());
+        response_intent.putExtra("action", "play");
+        response_intent.putExtra("event", "prepared");
+        localBroadcastManager.sendBroadcast(response_intent);
+
+        mp.start();
+        Log.d(LOG_TAG, "MediaPlayer prepared");
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Log.d(LOG_TAG, "MediaPlayer completed");
+        mp.release();
+
+        Intent response_intent = new Intent(VideoPlayer.class.getSimpleName());
+        response_intent.putExtra("action", "play");
+        response_intent.putExtra("event", "completed");
+        localBroadcastManager.sendBroadcast(response_intent);
     }
 }
+
